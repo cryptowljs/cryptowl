e74bf887387f490e9a045e9f066c4655b95369eb
Patricio López <lopezjuripatricio@gmail.com>
2017-09-05 15:33:25 -0300

    Add APILayer market service

--- packages/market-fiat/__tests__/APILayer.test.js
+++ packages/market-fiat/__tests__/APILayer.test.js
@@ -1,7 +1,24 @@
+const dotenv = require("dotenv");
 const { APILayer } = require("../src");
 
 describe("APILayer", () => {
+  beforeAll(() => {
+    dotenv.config();
+  });
+
   it("exists", async () => {
     expect(APILayer).toBeTruthy();
+  });
+
+  it("get currencies", async () => {
+    const service = new APILayer({ apiKey: process.env["CURRENCYLAYER__KEY"] });
+    const currencies = await service.getCurrencies();
+    expect(currencies).toBeInstanceOf(Array);
+  });
+
+  it("get current tick state", async () => {
+    const service = new APILayer({ apiKey: process.env["CURRENCYLAYER__KEY"] });
+    const data = await service.getCurrent(["CLP", "USD", "EUR"]);
+    expect(data).toBeTruthy();
   });
 });
--- packages/market-fiat/src/APILayer.js
+++ packages/market-fiat/src/APILayer.js
@@ -1,5 +1,80 @@
+const axios = require("axios");
+const ms = require("millisecond");
+
+const Observable = require("rxjs/Observable").Observable;
+require("rxjs/add/observable/interval");
+require("rxjs/add/operator/startWith");
+require("rxjs/add/operator/mergeMap");
+
+const parse = require("@cryptolw/money-parse");
+const CryptowlError = require("@cryptolw/error");
+
 class APILayer {
-  constructor() {}
+  static identifier = "APILayer";
+
+  static defaults = {
+    baseURL: "http://apilayer.net/api/",
+    timeout: 5000,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+
+  constructor(options) {
+    this.options = Object.assign({}, this.constructor.defaults, options);
+    this.client = axios.create(this.options);
+  }
+
+  getCurrencies() {
+    const params = {
+      access_key: this.options.apiKey,
+    };
+    return this.client.get("/list", { params }).then(({ data }) => {
+      return Object.keys(data["currencies"]);
+    });
+  }
+
+  getCurrent(currencies = [], options = {}) {
+    const params = {
+      access_key: this.options.apiKey,
+      source: options.from || "USD",
+      currencies: currencies.join(","),
+      format: 0,
+    };
+    return this.client
+      .get("/live", { params })
+      .then(({ data }) => {
+        if (data["success"]) {
+          return data;
+        } else {
+          throw new CryptowlError("Error querying apilayer.net", data);
+        }
+      })
+      .then(data => {
+        const source = data["source"]; // equal to options["source"]
+        return Object.entries(data["quotes"]).map(([currency, value]) => {
+          return {
+            code: currency.replace(source, ""),
+            rate: parse(1 / value, source),
+            raw: options.raw === true ? value : undefined,
+          };
+        });
+      })
+      .catch(err => {
+        if (err.response || err.request) {
+          throw CryptowlError.fromAxiosError(err, "Error querying CoinCap.", { currencies });
+        } else {
+          throw err;
+        }
+      });
+  }
+
+  ticker$(currencies = [], options = {}) {
+    return Observable.interval(ms(options.interval || "1 min"))
+      .startWith(0)
+      .mergeMap(() => this.getCurrent(currencies, options));
+  }
 }
 
 module.exports = APILayer;
