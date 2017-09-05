7d280d6f54f30e72e4d8041018a6a186656e34c8
Patricio López <lopezjuripatricio@gmail.com>
2017-09-05 15:32:43 -0300

    Add CoinCap market service

--- packages/market-crypto/__tests__/CoinCap.test.js
+++ packages/market-crypto/__tests__/CoinCap.test.js
@@ -4,4 +4,16 @@ describe("CoinCap", () => {
   it("exists", async () => {
     expect(CoinCap).toBeTruthy();
   });
+
+  it("get currencies", async () => {
+    const service = new CoinCap();
+    const currencies = await service.getCurrencies();
+    expect(currencies).toBeInstanceOf(Array);
+  });
+
+  it("get current tick state", async () => {
+    const service = new CoinCap();
+    const data = await service.getCurrent(["BTC", "ETH"]);
+    expect(data).toBeTruthy();
+  });
 });
--- packages/market-crypto/src/CoinCap.js
+++ packages/market-crypto/src/CoinCap.js
@@ -1,5 +1,70 @@
+const axios = require("axios");
+const ms = require("millisecond");
+
+const Observable = require("rxjs/Observable").Observable;
+require("rxjs/add/observable/interval");
+require("rxjs/add/operator/startWith");
+require("rxjs/add/operator/mergeMap");
+
+const parse = require("@cryptolw/money-parse");
+const CryptowlError = require("@cryptolw/error");
+
 class CoinCap {
-  constructor() {}
+  static identifier = "CoinCap";
+
+  static defaults = {
+    baseURL: "https://www.coincap.io/",
+    timeout: 5000,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+
+  constructor(options) {
+    this.options = Object.assign({}, this.constructor.defaults, options);
+    this.client = axios.create(this.options);
+  }
+
+  getCurrencies() {
+    return this.client.get("/coins").then(({ data }) => data);
+  }
+
+  getCurrent(currencies = [], options = {}) {
+    return this.client
+      .get("/front")
+      .then(({ data }) => {
+        // TODO: performance can be improved (filtering).
+        const result = {};
+        for (const object of data) {
+          const code = object["short"].toUpperCase();
+          result[code] = {
+            code,
+            name: object["long"],
+            supply: parse(object["supply"], code),
+            volume: parse(object["volume"], code),
+            rate: parse(object["price"], "USD"),
+            cap: parse(object["mktcap"], "USD"),
+            change: object["perc"],
+            raw: options.raw === true ? object : undefined,
+          };
+        }
+        return currencies.map(code => result[code]).filter(Boolean);
+      })
+      .catch(err => {
+        if (err.response || err.request) {
+          throw CryptowlError.fromAxiosError(err, "Error querying CoinCap.", { currencies });
+        } else {
+          throw err;
+        }
+      });
+  }
+
+  ticker$(currencies = [], options = {}) {
+    return Observable.interval(ms(options.interval || "1 min"))
+      .startWith(0)
+      .mergeMap(() => this.getCurrent(currencies, options));
+  }
 }
 
 module.exports = CoinCap;
